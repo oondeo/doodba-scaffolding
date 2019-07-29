9856463d125d410e8a222fbce37559d6d6185f57
Amon-Ra <amon.raj@gmail.com>
2019-07-29 19:10:58 +0200

    Update

--- odoo/custom/entrypoint.d/okd.sh
+++ odoo/custom/entrypoint.d/okd.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 if [ `id -u` -ge 10000 ]; then
+    umask 0002
     if ! whoami &> /dev/null; then
         if [ -w /etc/passwd ]; then
-            echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
+            sed "/^${USER_NAME:-odoo}:/d" /etc/passwd > /tmp/.passwd
+            echo "${USER_NAME:-odoo}:x:$(id -u):0:${USER_NAME:-odoo} user:${HOME}:/sbin/nologin" >> /tmp/.passwd
+            cat /tmp/.passwd > /etc/passwd
+            rm /tmp/.passwd
         fi
     fi
 fi
